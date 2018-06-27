6625a71f237d960963061084b2e451c01d50bb0f
ivangaru <40645274+ivangaru@users.noreply.github.com>
2018-06-27 15:07:31 -0700

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,15 @@ namespace GradeBook.GradeBooks
         {
             this.Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5) throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            if (averageGrade >= 80) return 'A';
+            if (averageGrade >= 60) return 'B';
+            if (averageGrade >= 40) return 'C';
+            if (averageGrade >= 20) return 'D';
+            return 'F';
+        }
     }
 }
